7df423b28357d69b3004292da0bbc832ad3d7954
Tobias Schulte <tobias.schulte@gliderpilot.de>
2014-06-29 21:11:30 +0200

    tasks are registered in plugin

--- src/main/groovy/de/gliderpilot/gradle/jnlp/GradleJnlpPlugin.groovy
+++ src/main/groovy/de/gliderpilot/gradle/jnlp/GradleJnlpPlugin.groovy
@@ -2,12 +2,25 @@ package de.gliderpilot.gradle.jnlp
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 /**
  * This is the main plugin file. Put a description of your plugin here.
  */
 class GradleJnlpPlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.extensions.create('jnlp', GradleJnlpPluginExtension, this)
+        project.extensions.create('jnlp', GradleJnlpPluginExtension, this, project)
+
+        project.tasks.create('generateJnlp', JnlpTask) {
+            output new File(project.buildDir, 'tmp/jnlp/launch.jnlp')
+        }
+        project.tasks.create('copyJars', CopyJarsTask) {
+            into new File(project.buildDir, 'tmp/jnlp/lib')
+        }
+/*        project.tasks.create('signJars', SignJarsTask) {
+            dependsOn project.copyJars
+            from project.fileTree(new File(project.buildDir, 'tmp/jnlp/lib'))
+        }
+*/
     }
 }
