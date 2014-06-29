a3b4936df25073acb9a303f5b91b5edab93a308b
Tobias Schulte <tobias.schulte@gliderpilot.de>
2014-06-29 21:11:14 +0200

    New task to copy the jars

---
+++ src/main/groovy/de/gliderpilot/gradle/jnlp/CopyJarsTask.groovy
@@ -0,0 +1,26 @@
+package de.gliderpilot.gradle.jnlp
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.artifacts.ResolvedArtifact
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.TaskAction
+
+class CopyJarsTask extends DefaultTask {
+
+    @OutputDirectory
+    File into
+
+    @TaskAction
+    void copy() {
+        def resolvedJars = project.configurations.runtime.resolvedConfiguration.resolvedArtifacts.findAll { it.extension == 'jar' }
+        resolvedJars.each { ResolvedArtifact artifact ->
+            project.copy {
+                from artifact.file
+                into into
+                rename {
+                    "${artifact.name}__V${artifact.moduleVersion.id.version}.jar"
+                }
+            }
+        }
+    }
+}
